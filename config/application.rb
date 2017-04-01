1fd2ef9fcb3c47ea890a08272b3aee8a665038ab
William Cunningham <w.a.cunningham.ii@gmail.com>
2017-03-31 23:53:47 -0400

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,19 @@
+require_relative 'boot'
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
+Bundler.require(*Rails.groups)
+
+module NotesRails
+  class Application < Rails::Application
+    config.load_defaults 5.1
+  end
+end
