f89477694a48b6466da6cdb92c4d3bf9834ad26e
William Cunningham <w.a.cunningham.ii@gmail.com>
2017-05-02 23:54:42 -0400

    Fix followers and followed associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
   has_many :notes
   has_many :followed_follows, class_name: "Follow", foreign_key: "followed_id"
   has_many :follower_follows, class_name: "Follow", foreign_key: "follower_id"
-  has_many :followed, through: :follower_follows, source: :follower
-  has_many :followers, through: :followed_follows, source: :followed
+  has_many :followed, through: :follower_follows, source: :followed
+  has_many :followers, through: :followed_follows, source: :follower
 
   validates :username, presence: true
 
