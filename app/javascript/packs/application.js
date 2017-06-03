1d1e1a5908b4857378a9e4dcdacf172395976f8f
William Cunningham <w.a.cunningham.ii@gmail.com>
2017-06-02 20:58:50 -0400

    Do not persist current user id in client

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import thunk from "redux-thunk";
 import { offline } from "redux-offline";
 import offlineConfig from "redux-offline/lib/defaults";
 import NetworkError from "redux-offline/lib/defaults/effect";
+import createBlacklistFilter from 'redux-persist-transform-filter';
 import axios from "axios";
 
 import App from "./containers/app";
@@ -26,6 +27,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // FIXME: add redux tools back in
   // FIXME: extract and test changes to redux-offline
+  // FIXME: with the persistOptions blacklist
+  //  there is a delay before notes are initially displayed
+  //  perhaps rehydration is broken
   const store = createStore(
     rootReducer,
     compose(
@@ -36,7 +40,13 @@ document.addEventListener("DOMContentLoaded", () => {
             .then(response => response.data)
             .catch(response => new NetworkError(response.data, response.status))
         ),
-        persistOptions: { blacklist: ["form"] },
+        persistOptions: {
+          blacklist: ["form"],
+          transforms: [createBlacklistFilter(
+            "data",
+            ["currentUserId"]
+          )],
+        },
         persistCallback: () => {
           // TODO: don't repurpose existing actions here
 
