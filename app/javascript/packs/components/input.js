3e67c89c1a68ffe0efc2b2b5a9c8cff5dec50398
William Cunningham <w.a.cunningham.ii@gmail.com>
2017-06-08 21:57:40 -0400

    Migrate to prop-types package

--- app/javascript/packs/components/input.js
+++ app/javascript/packs/components/input.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
 function Input({ input, label, type, meta: { touched, error } }) {
   return (
